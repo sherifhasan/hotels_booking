00ad94bb61809f35f544c284ed2da234233c2ddc
sherifhasan <sherif.hasan1994@gmail.com>
2024-09-26 20:36:18 +0200

    Fixed analysis error

--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
 import 'package:hotels_booking/presentation/screens/bottom_nav_screen.dart';
 import 'package:hotels_booking/presentation/screens/favourites_screen.dart';
 import 'package:hotels_booking/presentation/screens/home_screen.dart';
