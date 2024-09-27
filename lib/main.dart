4693cfb38eba8270b5cd675b8602bfcb26e950ee
sherifhasan <sherif.hasan1994@gmail.com>
2024-09-27 18:19:53 +0200

    Added Favourite cubit in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hotels_booking/application/hotel_cubit.dart';
+import 'package:hotels_booking/application/favourite/favourite_cubit.dart';
 import 'package:hotels_booking/presentation/app_theme.dart';
 import 'package:hotels_booking/presentation/router/app_router.dart';
 
-import 'injection.dart';
+import 'application/hotel/hotel_cubit.dart';
+import 'setup.dart';
 
-void main() {
-  setupInjection();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setup();
   runApp(const MyApp());
 }
 
@@ -20,7 +22,10 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider(
           create: (_) => getIt<HotelCubit>()..fetchHotels(),
-        )
+        ),
+        BlocProvider(
+          create: (_) => getIt<FavouriteCubit>()..loadFavourites(),
+        ),
       ],
       child: MaterialApp.router(
         title: 'Hotels booking',
