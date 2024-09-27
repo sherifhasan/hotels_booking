2b3137c230958a8c826d547046a6f84e13841a49
sherifhasan <sherif.hasan1994@gmail.com>
2024-09-27 19:51:41 +0200

    Added Favourite cubit unit tests

---
+++ test/application/favourite/favourite_cubit_test.dart
@@ -0,0 +1,142 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hotels_booking/application/favourite/favourite_cubit.dart';
+import 'package:hotels_booking/domain/entities/hotel_entity.dart';
+import 'package:hotels_booking/domain/usecases/favourite/favourite_usecases.dart';
+import 'package:mocktail/mocktail.dart';
+
+// Mock classes for use cases
+class MockAddToFavouritesUseCase extends Mock
+    implements AddToFavouritesUseCase {}
+
+class MockRemoveFromFavouritesUseCase extends Mock
+    implements RemoveFromFavouritesUseCase {}
+
+class MockGetFavouritesUseCase extends Mock implements GetFavouritesUseCase {}
+
+void main() {
+  late FavouriteCubit favouriteCubit;
+  late MockAddToFavouritesUseCase mockAddToFavouritesUseCase;
+  late MockRemoveFromFavouritesUseCase mockRemoveFromFavouritesUseCase;
+  late MockGetFavouritesUseCase mockGetFavouritesUseCase;
+
+  setUp(() {
+    mockAddToFavouritesUseCase = MockAddToFavouritesUseCase();
+    mockRemoveFromFavouritesUseCase = MockRemoveFromFavouritesUseCase();
+    mockGetFavouritesUseCase = MockGetFavouritesUseCase();
+    favouriteCubit = FavouriteCubit(
+      mockGetFavouritesUseCase,
+      mockAddToFavouritesUseCase,
+      mockRemoveFromFavouritesUseCase,
+    );
+  });
+
+  tearDown(() {
+    favouriteCubit.close();
+  });
+
+  final hotel = HotelEntity(
+    hotelId: '1',
+    name: 'Hotel AluaVillage Blue Beach',
+    destination: 'Playa de Esquinzo, Fuerteventura, Spanien',
+    imageUrl: 'https://example.com/hotel.jpg',
+    ratingScore: 5.0,
+    scoreDescription: 'sehr gut',
+    days: 8,
+    nights: 7,
+    roomName: 'Doppelzimmer',
+    boarding: 'Frühstück',
+    adultCount: 2,
+    childrenCount: 2,
+    flightIncluded: true,
+    totalPrice: 1280,
+    pricePerPerson: 320,
+  );
+
+  group('FavouriteCubit Tests', () {
+    blocTest<FavouriteCubit, FavouriteState>(
+      'emits [loading, loaded] when loadFavourites is successful with data',
+      build: () {
+        when(() => mockGetFavouritesUseCase())
+            .thenReturn(Right([hotel])); // Use synchronous return here
+        return favouriteCubit;
+      },
+      act: (cubit) => cubit.loadFavourites(),
+      expect: () => [
+        const FavouriteState.loading(),
+        FavouriteState.loaded([hotel]),
+      ],
+      verify: (_) {
+        verify(() => mockGetFavouritesUseCase()).called(1);
+      },
+    );
+
+    blocTest<FavouriteCubit, FavouriteState>(
+      'emits [loading, empty] when loadFavourites returns empty list',
+      build: () {
+        when(() => mockGetFavouritesUseCase())
+            .thenReturn(const Right([])); // Return an empty list
+        return favouriteCubit;
+      },
+      act: (cubit) => cubit.loadFavourites(),
+      expect: () => [
+        const FavouriteState.loading(),
+        const FavouriteState.empty(),
+      ],
+      verify: (_) {
+        verify(() => mockGetFavouritesUseCase()).called(1);
+      },
+    );
+
+    blocTest<FavouriteCubit, FavouriteState>(
+      'emits [loaded] with hotel added when addFavourite is called',
+      build: () {
+        when(() => mockAddToFavouritesUseCase(hotel))
+            .thenAnswer((_) async => const Right(null));
+        return favouriteCubit..emit(FavouriteState.loaded([])); // Initial state
+      },
+      act: (cubit) => cubit.addFavourite(hotel),
+      expect: () => [
+        FavouriteState.loaded([hotel]),
+      ],
+      verify: (_) {
+        verify(() => mockAddToFavouritesUseCase(hotel)).called(1);
+      },
+    );
+
+    blocTest<FavouriteCubit, FavouriteState>(
+      'emits [loaded] with hotel removed when removeFavourite is called',
+      build: () {
+        when(() => mockRemoveFromFavouritesUseCase(any()))
+            .thenAnswer((_) async => const Right(null));
+        return favouriteCubit
+          ..emit(FavouriteState.loaded([hotel])); // Initial state
+      },
+      act: (cubit) => cubit.removeFavourite(hotel.hotelId),
+      expect: () => [
+        FavouriteState.loaded([]),
+      ],
+      verify: (_) {
+        verify(() => mockRemoveFromFavouritesUseCase(hotel.hotelId)).called(1);
+      },
+    );
+
+    blocTest<FavouriteCubit, FavouriteState>(
+      'emits [error] when loadFavourites fails',
+      build: () {
+        when(() => mockGetFavouritesUseCase())
+            .thenReturn(Left(Exception('Error'))); // Simulate failure
+        return favouriteCubit;
+      },
+      act: (cubit) => cubit.loadFavourites(),
+      expect: () => [
+        const FavouriteState.loading(),
+        const FavouriteState.error('Failed to load favourites'),
+      ],
+      verify: (_) {
+        verify(() => mockGetFavouritesUseCase()).called(1);
+      },
+    );
+  });
+}
