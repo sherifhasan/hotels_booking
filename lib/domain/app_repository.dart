f7b2f1055782e635466e175e1e85cd575997d01a
sherifhasan <sherif.hasan1994@gmail.com>
2024-09-27 18:14:01 +0200

    Added favourite methods in app repo

--- lib/domain/app_repository.dart
+++ lib/domain/app_repository.dart
@@ -5,4 +5,11 @@ import 'failure.dart';
 
 abstract class AppRepository {
   Future<Either<Failure, List<HotelEntity>>> fetchHotels();
+
+  // Favourites
+  Either<Exception, List<HotelEntity>> getFavourites();
+
+  Future<Either<Exception, void>> addToFavourites(HotelEntity hotel);
+
+  Future<Either<Exception, void>> removeFromFavourites(String hotelId);
 }
--- lib/infrastructure/app_repository_imp.dart
+++ lib/infrastructure/app_repository_imp.dart
@@ -2,13 +2,16 @@ import 'package:dartz/dartz.dart';
 import 'package:hotels_booking/domain/app_repository.dart';
 import 'package:hotels_booking/domain/entities/hotel_entity.dart';
 import 'package:hotels_booking/domain/failure.dart';
+import 'package:hotels_booking/infrastructure/datasources/favourite_local_data_source.dart';
+import 'package:hotels_booking/infrastructure/storage/hotel_database_model.dart';
 
 import 'datasources/hotels_remote_data_source.dart';
 
 class AppRepositoryImp implements AppRepository {
   final HotelRemoteDataSource remoteDataSource;
+  final FavouriteLocalDataSource favouriteLocalDataSource;
 
-  AppRepositoryImp(this.remoteDataSource);
+  AppRepositoryImp(this.remoteDataSource, this.favouriteLocalDataSource);
 
   @override
   Future<Either<Failure, List<HotelEntity>>> fetchHotels() async {
@@ -22,5 +25,35 @@ class AppRepositoryImp implements AppRepository {
         return Right(hotels); // Return success with HotelEntities
       },
     );
+  }
+
+  @override
+  Either<Exception, List<HotelEntity>> getFavourites() {
+    try {
+      final favourites = favouriteLocalDataSource.getFavourites();
+      return Right(favourites.map((hotel) => hotel.toEntity()).toList());
+    } catch (e) {
+      return Left(Exception('Failed to load favourites'));
+    }
+  }
+
+  @override
+  Future<Either<Exception, void>> addToFavourites(HotelEntity hotel) async {
+    try {
+      await favouriteLocalDataSource.addToFavourites(hotel.toHiveModel());
+      return Right(null);
+    } catch (e) {
+      return Left(Exception('Failed to add to favourites'));
+    }
+  }
+
+  @override
+  Future<Either<Exception, void>> removeFromFavourites(String hotelId) async {
+    try {
+      await favouriteLocalDataSource.removeFromFavourites(hotelId);
+      return Right(null);
+    } catch (e) {
+      return Left(Exception('Failed to remove from favourites'));
+    }
   }
 }
