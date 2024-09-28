133f6b2a8ce461e20b7d7d1a2fb10c3d2883994a
sherifhasan <sherif.hasan1994@gmail.com>
2024-09-28 18:38:01 +0200

    Fixed favorite cubit to correctly handle add and remove

--- lib/application/favourite/favourite_cubit.dart
+++ lib/application/favourite/favourite_cubit.dart
@@ -4,7 +4,6 @@ import 'package:hotels_booking/domain/entities/hotel_entity.dart';
 import 'package:hotels_booking/domain/usecases/favourite/favourite_usecases.dart';
 
 part 'favourite_state.dart';
-
 part 'favourite_cubit.freezed.dart';
 
 class FavouriteCubit extends Cubit<FavouriteState> {
@@ -13,43 +12,51 @@ class FavouriteCubit extends Cubit<FavouriteState> {
   final GetFavouritesUseCase getFavourites;
 
   FavouriteCubit(
-    this.getFavourites,
-    this.addToFavourites,
-    this.removeFromFavourites,
-  ) : super(const FavouriteState.loading());
+      this.getFavourites,
+      this.addToFavourites,
+      this.removeFromFavourites,
+      ) : super(const FavouriteState.loading());
 
   Future<void> loadFavourites() async {
     emit(const FavouriteState.loading());
     final result = getFavourites();
     result.fold(
-      (error) => emit(FavouriteState.error('Failed to load favourites')),
-      (favourites) => emit(favourites.isEmpty
-          ? const FavouriteState.empty()
-          : FavouriteState.loaded(favourites)),
+          (error) => emit(FavouriteState.error('Failed to load favourites')),
+          (favourites) => favourites.isEmpty
+          ? emit(const FavouriteState.empty())
+          : emit(FavouriteState.loaded(favourites)),
     );
   }
 
   Future<void> addFavourite(HotelEntity hotel) async {
     state.maybeWhen(
       loaded: (favourites) {
-        // Optimistically update the UI by adding the hotel to the state
-        emit(FavouriteState.loaded([...favourites, hotel]));
+        final updatedFavourites = [...favourites, hotel];
+        emit(FavouriteState.loaded(updatedFavourites));
+      },
+      empty: () {
+        // If currently empty, move to loaded state when a favorite is added
+        emit(FavouriteState.loaded([hotel]));
       },
       orElse: () {},
     );
-    await addToFavourites(hotel); // Save to repository
+    await addToFavourites(hotel); // Save to local storage/database
   }
 
   Future<void> removeFavourite(String hotelId) async {
     state.maybeWhen(
       loaded: (favourites) {
-        // Optimistically update the UI by removing the hotel from the state
-        emit(FavouriteState.loaded(
-            favourites.where((hotel) => hotel.hotelId != hotelId).toList()));
+        final updatedFavourites =
+        favourites.where((hotel) => hotel.hotelId != hotelId).toList();
+        if (updatedFavourites.isEmpty) {
+          emit(const FavouriteState.empty());
+        } else {
+          emit(FavouriteState.loaded(updatedFavourites));
+        }
       },
       orElse: () {},
     );
-    await removeFromFavourites(hotelId); // Remove from repository
+    await removeFromFavourites(hotelId); // Remove from local storage/database
   }
 
   bool isFavourite(String hotelId) {
